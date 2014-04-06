862c603ba5ecbf5ab5a728eebc418330c7ecdfd3
bornold <jonas.bornold@gmail.com>
2014-04-06 13:18:44 +0200

    added ctrl+z regret on removed nodes

--- nodemore.js
+++ nodemore.js
@@ -1,20 +1,39 @@
 var targets = "*:not(head, html)"
 var ctrlDown = false;
 var altDown = false;
-
+var hidden = [];
+//var bgcolor;
 $(document).ready(function(){
    $(targets).click(function(e){
     if (ctrlDown && altDown && (e.target == this)) {
-        e.preventDefault();
         e.stopImmediatePropagation();
+        e.preventDefault();
         removeDiv($(this));
         return false;
     }
   });
 });
+/*
+$( targets )
+  .mouseover(function(e) {
+     if (ctrlDown && altDown && (e.target == this)) {
+//            bgcolor = $( this ).css( "background-color" );
+//            $( this ).css( "background-color", "red" );
+            $( this ).fadeTo("fast", 0.33);
+
+    }
+  })
+  .mouseout(function(e) {
+     if ($( this ).is(":visible") ) {
+//            $( this ).css( "background-color", bgcolor );
+            $( this ).fadeTo("fast", 1);
+     }
+  });
+*/
 
 // hide or remove?
 function removeDiv(div) {
+    hidden.push(div);
     div.hide();
 }
 
@@ -24,6 +43,8 @@ $(window).bind('keydown', function(e){
         ctrlDown = true;
     } else if(e.keyCode == 18) { //alt
            altDown = true;
+    } else if(ctrlDown && e.keyCode == 90 && hidden.length > 0) { // show last hidden
+        hidden.pop().show();
     }
 });
 
