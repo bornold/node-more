c15479f51d431ff9ef07f02f16259d9ca26d2e17
bornold <jonas.bornold@gmail.com>
2014-04-07 12:09:21 +0200

    elelment that will be removed fades when ctrl+alt is hold and hooverd

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 node-more
 =========
+A small extension that lets you remove nodes by holding ctrl+alt and clicking them.
+Needs much more functionality.
+
+- keymapping for macs
+- preventdefault does not work on most ads, so need to look in to that
+- better way to see what element will be removed.
--- nodemore.js
+++ nodemore.js
@@ -1,59 +1,71 @@
-var targets = "*:not(head, html)"
+/* To remove the script later
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-2.1.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+*/
+// the nodes that are removelbe
+var targets = "*:not(head, html)";
+
+// if key down booleans
 var ctrlDown = false;
 var altDown = false;
+
+// key mappings
+var ctrl = 17;
+var alt = 18;
+var z = 90;
+
+// array of recently hidden elements
 var hidden = [];
-//var bgcolor;
+
 $(document).ready(function(){
-   $(targets).click(function(e){
-    if (ctrlDown && altDown && (e.target == this)) {
-        e.stopImmediatePropagation();
-        e.preventDefault();
-        removeDiv($(this));
-        return false;
-    }
-  });
+    $(targets)
+        .click(function(e){
+           if (ctrlDown && altDown && (e.target == this)) {
+                e.preventDefault(); // to prevent links from firing
+                e.stopImmediatePropagation(); // this to
+                removeNode( $( this ) );
+                return false; // and this, don't works all of the time thou
+            }
+        })
+        .mouseenter(function(e) {
+            if (ctrlDown && altDown && (e.target == this)) {
+                $( this ).fadeTo("fast", 0.33);
+            }
+        })
+        .mouseout(function(e) {
+            if ($( this ).is(":visible") ) {
+                $( this ).fadeTo("fast", 1);
+            }
+        });
+    $(window)
+        .bind('keydown', function(e){
+            if(e.keyCode == ctrl ) { //ctrl
+                ctrlDown = true;
+            } else if(e.keyCode == alt) { //alt
+               altDown = true;
+            } else if ( ctrlDown && e.keyCode == z && hidden.length > 0) {
+               hidden.pop().show(); // show last hidden
+            }	
+        })
+        .bind('keyup', function(e){
+            if(e.keyCode == ctrl ) { //ctrl
+                ctrlDown = false;
+            } else if(e.keyCode == alt) { //alt
+                altDown = false;
+            }
+        })
+        .focusout(function() {
+            ctrlDown = false;
+            altDown = false;
+        });
 });
-/*
-$( targets )
-  .mouseover(function(e) {
-     if (ctrlDown && altDown && (e.target == this)) {
-//            bgcolor = $( this ).css( "background-color" );
-//            $( this ).css( "background-color", "red" );
-            $( this ).fadeTo("fast", 0.33);
 
-    }
-  })
-  .mouseout(function(e) {
-     if ($( this ).is(":visible") ) {
-//            $( this ).css( "background-color", bgcolor );
-            $( this ).fadeTo("fast", 1);
-     }
-  });
+function removeNode(node) {
+ /*
+    hidden.push(node);
+    node.hide();
 */
-
-// hide or remove?
-function removeDiv(div) {
-    hidden.push(div);
-    div.hide();
+    node.remove();
 }
-
-// toggle if keydown
-$(window).bind('keydown', function(e){
-    if(e.keyCode == 17 ) { //ctrl
-        ctrlDown = true;
-    } else if(e.keyCode == 18) { //alt
-           altDown = true;
-    } else if(ctrlDown && e.keyCode == 90 && hidden.length > 0) { // show last hidden
-        hidden.pop().show();
-    }
-});
-
-// toggle if key up
-$(window).bind('keyup', function(e){
-    if(e.keyCode == 17 ) { //ctrl
-        ctrlDown = false;
-    } else if(e.keyCode == 18) { //alt
-       altDown = false;
-    }
-});
-
